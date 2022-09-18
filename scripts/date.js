f6f6ec462f34f8636b3c4e938e3a5a25e8d259a2
elinaee <elissa@live.com.pt>
2022-09-18 05:19:14 +0200

    update

--- scripts/date.js
+++ scripts/date.js
@@ -1,43 +1,6 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const year = document.querySelector('#year');
 
-    function date() {
-       year.innerHTML = new Date().getFullYear();
-    };
 
-    date();
-}
 
-// *********************************************
-
-let dayname = [
-"Sunday",
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday"
-];
-
-let months = [
-"January",
-"February",
-"March",
-"April",
-"May",
-"June",
-"July",
-"August",
-"September",
-"October",
-"November",
-"December"
-];
-
-let d = new Date();
-let dayName = daynames[d.getDay()];
-let monthName = months[d.getMonth()];
-let fulldate = dayName +", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
-
-document.getElementById("currentdate").textcontent = fulldate;
+date = new Date().getFullYear();
+document.querySelector("#year").innerHTML = date
+document.querySelector("#timestamp").innerHTML= document.lastModified;
