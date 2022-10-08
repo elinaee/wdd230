5a47f83e6689845a3909aed874c6e1642cc9c55e
elinaee <elissa@live.com.pt>
2022-10-08 17:27:12 +0200

    new

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,5 +1,24 @@
+// Date that goes on the footer
 const options = {year: 'numeric'};
 document.getElementById('date').textContent = new Date ().toLocaleDateString('en-US', options);
 
+// Date that goes on the Header 
 const dateTime = document.lastModified;
 document.querySelector('#lastMod').textContent  = `Last Updated: ${dateTime}`;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
