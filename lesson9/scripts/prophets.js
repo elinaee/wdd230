fe89d3e8e7e21357bee5b496cfec9b40c415d383
elinaee <elissa@live.com.pt>
2022-11-13 08:33:10 +0100

    new

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -35,9 +35,9 @@ function displayProphets(prophet) {
     // Build the image attributes by using the setAttribute 
     //method for the src, alt, and loading attribute values. 
     //(Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
-    ____.setAttribute('loading', 'lazy');
+    cards.setAttribute('src', prophet.imageurl);
+    cards.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+    cards.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
@@ -45,8 +45,7 @@ function displayProphets(prophet) {
     card.appendChild(parag);
     card.appendChild(parag1);
   
-    // Add/append the existing HTML div with the cards class 
-    //with the section(card)
+
     document.querySelector('div.cards').appendChild(card);
 };
 
